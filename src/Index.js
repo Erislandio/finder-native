81055e58f2159ab404736c330c8cc17366717571
erislandio.soares <erislandio.soares@acct.global>
2020-03-09 15:28:53 -0300

    ajustes template de rotas

--- src/Index.js
+++ src/Index.js
@@ -1,14 +1,27 @@
-import { createStackNavigator } from 'react-navigation-stack';
-import { createAppContainer } from 'react-navigation';
+// In App.js in a new project
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 import { LoginScreen } from './screens/Login/Login';
 
-export const Routes = createAppContainer(
-	createStackNavigator({
-		Login: {
-			screen: LoginScreen,
-			navigationOptions: {
-				headerShown: false
-			}
-		}
-	})
-);
+const Stack = createStackNavigator();
+
+function Routes() {
+	return (
+		<NavigationContainer>
+			<Stack.Navigator initialRouteName="/login">
+				<Stack.Screen
+					name="/login"
+					component={LoginScreen}
+					options={{
+						headerShown: false,
+						animationEnabled: true
+					}}
+				/>
+			</Stack.Navigator>
+		</NavigationContainer>
+	);
+}
+
+export { Routes };
