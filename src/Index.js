e7601844611ca9e589171a403475d685b8ea51d8
erislandio <erislandiosoares21@gmail.com>
2020-03-14 15:35:46 -0300

    Adiçao autenticaçao do usuario

--- src/Index.js
+++ src/Index.js
@@ -6,49 +6,58 @@ import { createStackNavigator } from "@react-navigation/stack";
 import LoginScreen from "./screens/Login/Login";
 import { HomeScreen } from "./screens/Home/Home";
 import { SignInScreen } from "./screens/SignIn/SignInScreen";
+import { AsyncStorage } from "react-native";
 
 const Stack = createStackNavigator();
 
 function Routes() {
+  const user = AsyncStorage.getItem("user").then(res => res);
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
-        <Stack.Screen
-          name="login"
-          component={LoginScreen}
-          options={{
-            animationEnabled: true,
-            animationTypeForReplace: "push",
-            cardShadowEnabled: true,
-            gestureDirection: "horizontal",
-            gestureEnabled: true,
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="home"
-          component={HomeScreen}
-          options={{
-            animationEnabled: true,
-            animationTypeForReplace: "push",
-            cardShadowEnabled: true,
-            gestureDirection: "horizontal",
-            gestureEnabled: true,
-            headerShown: false
-          }}
-        />
-        <Stack.Screen
-          name="signin"
-          component={SignInScreen}
-          options={{
-            animationEnabled: true,
-            animationTypeForReplace: "push",
-            cardShadowEnabled: true,
-            gestureDirection: "horizontal",
-            gestureEnabled: true,
-            headerShown: false
-          }}
-        />
+      <Stack.Navigator initialRouteName={user !== null ? "home" : "login"}>
+        {user ? (
+          <Stack.Screen
+            name="home"
+            component={HomeScreen}
+            options={{
+              animationEnabled: true,
+              animationTypeForReplace: "push",
+              cardShadowEnabled: true,
+              gestureDirection: "horizontal",
+              gestureEnabled: true,
+              headerShown: false
+            }}
+          />
+        ) : (
+          <React.Fragment>
+            <Stack.Screen
+              name="login"
+              component={LoginScreen}
+              options={{
+                animationEnabled: true,
+                animationTypeForReplace: "push",
+                cardShadowEnabled: true,
+                gestureDirection: "horizontal",
+                gestureEnabled: true,
+                headerShown: false
+              }}
+            />
+
+            <Stack.Screen
+              name="signin"
+              component={SignInScreen}
+              options={{
+                animationEnabled: true,
+                animationTypeForReplace: "push",
+                cardShadowEnabled: true,
+                gestureDirection: "horizontal",
+                gestureEnabled: true,
+                headerShown: false
+              }}
+            />
+          </React.Fragment>
+        )}
       </Stack.Navigator>
     </NavigationContainer>
   );
